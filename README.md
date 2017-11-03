2c262c8dcea41af7c839d697758bfd914cc79234
xschen <xs0040@gmail.com>
2017-11-03 09:51:22 +0800

    sample codes

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # cs-statistics
+
 A inferential statistical library implemented in C#
+
+# Install
+
+```bash
+Install-Package cs-statistics -Version 1.0.2
+```
+
+# Usage
+
+The sample codes can be found in the sample project cs-statistics-samples
+
+
---
+++ cs-statistics-samples/FTPercentileFunction.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Statistics.SpecialFunctions;
+using Statistics;
+
+namespace Statistics.FT
+{
+    public class FTPercentileFunction
+    {
+        public static void RunExample()
+        {
+            double q = 1800;
+            double mu = 1500;
+            double sigma = 300;
+            double p = Gaussian.GetPercentile(q, mu, sigma);
+            Console.WriteLine("p = {0}", p);
+
+            Console.WriteLine("p = {0}", Gaussian.GetPercentile(50, 45, 3.2));
+        }
+    }
+}
